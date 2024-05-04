449dfe4bc7b04df46a0bde20f5bc4656485a2510
JT <www.jamie.top@gmail.com>
2024-05-04 13:23:31 +0800

    fix: threads, tests

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -13,10 +13,10 @@ use std::time::{Duration, Instant};
 
 fn main() {
     let mut handles = vec![];
-    for i in 0..1000 {
+    for i in 0..10 {
         handles.push(thread::spawn(move || {
             let start = Instant::now();
-            thread::sleep(Duration::from_millis(2500));
+            thread::sleep(Duration::from_millis(250));
             println!("thread {} is complete", i);
             start.elapsed().as_millis()
         }));
